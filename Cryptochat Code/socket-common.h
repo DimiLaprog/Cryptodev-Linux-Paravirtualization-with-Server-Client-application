6eae4e2a1f2339f9441726c85b1c05a1f7c66f67
DimiLaprog <56197365+DimiLaprog@users.noreply.github.com>
2021-02-05 06:13:16 +0200

    Server-Client and Crypto API

---
+++ Cryptochat Code/socket-common.h
@@ -0,0 +1,19 @@
+/*
+ * socket-common.h
+ *
+ * Simple TCP/IP communication using sockets
+ *
+ * Vangelis Koukis <vkoukis@cslab.ece.ntua.gr>
+ */
+
+#ifndef _SOCKET_COMMON_H
+#define _SOCKET_COMMON_H
+
+/* Compile-time options */
+#define TCP_PORT    35001
+#define TCP_BACKLOG 5
+
+#define HELLO_THERE "Hello there!"
+
+#endif /* _SOCKET_COMMON_H */
+
